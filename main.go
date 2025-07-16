ca722e63cbe453c96cb69814593cedae6bf6bb4b
joe-paul-martin <joepaul.martin1448@gmail.com>
2025-07-16 19:08:21 +0530

    updated hello server code

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
